eb194684800cd459feec100c8abbd9dd2c692246
dcca <dcca12@gmail.com>
2020-06-21 17:00:06 -0300

    add vue clit to project

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <div>
+        <router-link tag="a" to="/">Home</router-link>
+        <router-link tag="a" to="/about">About</router-link>
+      </div>
+
+      <div>
+        <router-link tag="a" to="/about">Sign Up</router-link>
+        <router-link tag="a" to="/about">Log In</router-link>
+      </div>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  margin: 0 auto;
+  max-width: 1250px;
+  min-height: 100vh;
+}
+
+#nav {
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+}
+
+a {
+  text-decoration: none;
+  padding: 0 1rem;
+}
+</style>
